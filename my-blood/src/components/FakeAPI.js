0a7eb9142754a279af1e8c3b528d97aa4e7f5cd2
Ewelina <ewe.mrowczynska@wp.pl>
2022-07-30 11:05:42 +0200

    login

--- my-blood/src/components/FakeAPI.js
+++ my-blood/src/components/FakeAPI.js
@@ -2,11 +2,11 @@ const FakeAPI = {
     login: data => {
         const {username, password} = data;
         return new Promise((resolve, reject) => {
-            if (username === "Eve Mrr" && password === "qwer") {
+            if (username === "John Doe" && password === "start") {
                 resolve({
                     username,
-                    name: "Eve",
-                    surname: "Mrr",
+                    name: "John",
+                    surname: "Doe",
                     lastLogin: new Date("02/04/2022 21:37:00")
                 });
             } else {
